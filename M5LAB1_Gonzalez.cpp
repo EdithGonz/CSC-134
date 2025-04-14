e137b78671513fc51cc644443836d90ab6a74fcc
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-04-14 01:43:00 +0000

    IP

---
+++ M5LAB1_Gonzalez.cpp
@@ -0,0 +1,32 @@
+//CSC 134
+//M5LAB1
+//Edith Gonzalez
+//04-13-25
+
+#include <iostream>
+using namespace std;
+
+void printResult(int number, int result) {
+    cout << number << " sqaured = " << result << endl;
+}
+
+int square(int number) {
+    int result;
+    result = number * number;
+    return result;
+}
+
+
+int main ()
+{
+    int count = 1;
+    int result;
+
+    while (count <= 10){
+       
+        result = square(count);
+       
+        printResult(count, result);
+        count++;
+    }
+}
