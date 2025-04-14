2b2b233ca6fa172f3210edc07b32961408cfa5c2
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-04-14 01:21:51 +0000

    Done!

--- M5T2_Gonzalez.cpp
+++ M5T2_Gonzalez.cpp
@@ -1,39 +1,32 @@
 // CSC-134
-// M5T1
+// M5T2
 // Edith Gonzalez
 // 04-12-25
 
 #include <iostream>
 using namespace std;
 
-void say_hello();
-int give_the_number();
-int double_a_number (int num);
+void printResult(int number, int result) {
+    cout << number << " sqaured = " << result << endl;
+}
+
+int square(int number) {
+    int result;
+    result = number * number;
+    return result;
+}
+
 
 int main ()
 {
-    
-    cout << "Hello from main ()" << endl;
-    say_hello();
-    int my_answer = give_the_number();
-    cout << "the answer is: " << my_answer << endl;
-    int twotimes = double_a_number(7);
-    cout << "Here's another number: " << twotimes << endl;
-    return 0;
-     
-}
+    int count = 1;
+    int result;
 
-void say_hello(){
-    cout << "Hi from say_hello () " << endl;
-    return;
-
-}
-
-int give_the_number(){
-    return 42;
-}
-
-int double_a_number (int num){
-    int new_num = num * 2;
-    return new_num;
+    while (count <= 10){
+       
+        result = square(count);
+       
+        printResult(count, result);
+        count++;
+    }
 }
