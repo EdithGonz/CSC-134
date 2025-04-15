0224b00524773b05df7a3f1e56b2953f281c4047
EdithGonz <gonzalee4960@student.faytechcc.edu>
2025-04-15 06:09:36 +0000

    practice

---
+++ practiceboard.cpp
@@ -0,0 +1,19 @@
+/* THIS IS JUST A PRACTICE PAGE YUH ! */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+
+    double balance1;
+    double balance2;
+    double amount;
+    
+
+
+
+
+
+
+
+}
